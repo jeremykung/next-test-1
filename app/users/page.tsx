02e34ecf3de56a5a41fbd72a39a1eb829b8ba63a
es <es@ess-MacBook-Air.local>
2023-12-17 02:37:45 +0200

    added routes, data fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,28 @@
 import React from "react"
 
-const UsersPage = () => {
-  return <div>UsersPage</div>
+interface User {
+  id: number
+  name: string
+}
+
+const UsersPage = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const data = await response.json()
+  console.log("data", data)
+  const users: User[] = data
+
+  // .then((response) => response.json())
+  // .then((json) => console.log(json))
+
+  return (
+    <div>
+      <div className="users">
+        {users.map((user) => (
+          <p key={user.id}>{user.name}</p>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default UsersPage
